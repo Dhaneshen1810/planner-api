c9ebf8df83944fcee3748210492ef7b211410532
Dhaneshen1810 <dan.moonian@gmail.com>
2025-02-27 20:31:48 -0700

    fix rust handling get task by date

--- service/src/query.rs
+++ service/src/query.rs
@@ -37,15 +37,10 @@ impl Query {
         let query = Statement::from_sql_and_values(
             DbBackend::Postgres,
             r#"
-            SELECT id, 
-                   title, 
-                   date, 
-                   time, 
-                   recurring_option::recurring_option[] AS recurring_option, 
-                   is_completed, 
-                   position
+            SELECT id, title, date, time, recurring_option, is_completed, position
             FROM tasks 
-            WHERE (date IS NULL OR date = $1::date) OR $2::recurring_option = ANY(recurring_option)
+            WHERE (date IS NULL OR date = $1::date)
+              OR $2::text = ANY(recurring_option::text[])
             "#,
             vec![date.into(), weekday.into()],
         );
